dcb934576d042c640b382d1648f9c98740fc75bb
Gurpreet Raju <gurpreetraj92@gmail.com>
2017-02-28 20:00:01 -0500

    MVC implementation
    Created Model, Controller and View package, added some model classes to
    model package

---
+++ src/Model/Login.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Login {
+	private String UsrName;
+	private String UserID;
+	private String password;
+	
+	public boolean authenticate(){
+		
+		return true;
+	}
+}
---
+++ src/Model/Stock.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Stock {
+	private String companyName;
+	
+	public Stock(String name){
+		this.companyName = name;
+	}
+	
+	public String getCompanyName(){
+		return this.companyName;
+	}
+}
