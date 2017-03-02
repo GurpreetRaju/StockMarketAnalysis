ff105179924e98f2b386412c87fe7c38b8f932f7
Nikita Baranov <NikitaBaranov@gmail.com>
2017-03-02 06:16:01 +0500

    Add jfxrt.jar and chart example (from StackOverflow).

--- src/JavaFXTry.java
+++ src/JavaFXTry.java
@@ -1,22 +1,102 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
 
 public class JavaFXTry extends Application {
+	
+//    private DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE ;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy MM dd");
+
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Date");
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Value");
+
+        ObservableList<Data<String, Number>> data = FXCollections.observableArrayList();
+        
+        SortedList<Data<String, Number>> sortedData = new SortedList<>(data, (data1, data2) -> 
+                data1.getXValue().compareTo(data2.getXValue()));
+
+        LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
+
+        XYChart.Series series = new XYChart.Series(sortedData);
+        series.setName("Stock");
+        chart.getData().add(series);
+        
+//        chart.getData().add(new Series<>(sortedData));
+        chart.setAnimated(true);
+
+        final int dayRange = 30 ;
+        LocalDate today = LocalDate.now() ;
+        Random rng = new Random();
+
+        for (int i = 0; i < 20 ; i++) {
+            LocalDate date = today.minusDays(rng.nextInt(dayRange));
+            String formattedDate = formatter.format(date);
+            double value = rng.nextDouble() ;
+
+            addData(data, formattedDate, value);
+        }
+
+        DatePicker datePicker = new DatePicker();
+        Spinner<Double> valuePicker = new Spinner<>(0.0, 1.0, 0, 0.1);
+        valuePicker.setEditable(true);
+
+        Button addButton = new Button("Add");
+        addButton.setOnAction(e -> addData(data, formatter.format(datePicker.getValue()), valuePicker.getValue()));
+
+        HBox controls = new HBox(5, datePicker, valuePicker, addButton);
+        controls.setAlignment(Pos.CENTER);
+        controls.setPadding(new Insets(5));
+
+        BorderPane root = new BorderPane(chart, null, null, controls, null);
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+
+    private void addData(ObservableList<Data<String, Number>> data, String formattedDate, double value) {
+        Data<String, Number> dataAtDate = data.stream()
+            .filter(d -> d.getXValue().equals(formattedDate))
+            .findAny()
+            .orElseGet(() -> {
+                Data<String, Number> newData = new Data<String, Number>(formattedDate, 0.0);
+                data.add(newData);
+                return newData ;
+            }) ;
+        dataAtDate.setYValue(dataAtDate.getYValue().doubleValue() + value);
     	
+/*    	
         primaryStage.setTitle("Hello World!");
 
         Button btn = new Button();
@@ -31,12 +111,14 @@ public class JavaFXTry extends Application {
         
 
       //defining the axes
-        final NumberAxis xAxis = new NumberAxis();
+        final DateAxis xAxis = new DateAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("Number of Month");
+        yAxis.setLabel("Date");
+        
         //creating the chart
-        final LineChart<Number,Number> lineChart = 
-                new LineChart<Number,Number>(xAxis,yAxis);
+        final LineChart<Date,Number> lineChart = 
+                new LineChart<Date,Number>(xAxis,yAxis);
                 
         lineChart.setTitle("Stock Monitoring, 2010");
         //defining a series
@@ -59,9 +141,10 @@ public class JavaFXTry extends Application {
         lineChart.getData().add(series);
         
         StackPane root = new StackPane();
-        root.getChildren().addAll(lineChart,btn);
+        root.getChildren().add(lineChart);
         
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
+*/
     }
 }
