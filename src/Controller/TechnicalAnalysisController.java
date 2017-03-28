d6377323ee5d7910ed468c916cbf5b5dd67740b5
Nikita Baranov <NikitaBaranov@gmail.com>
2017-03-29 04:33:29 +0500

    Separate viev and controller. Watch list with icons. Menu File -> Exit.

--- src/Controller/TechnicalAnalysisController.java
+++ src/Controller/TechnicalAnalysisController.java
@@ -12,53 +12,85 @@ import org.jfree.chart.JFreeChart;
 import Model.TechnicalAnalysis;
 import View.TechnicalAnalysisView;
 
+import javax.swing.*;
+
 public class TechnicalAnalysisController {
 	
 	private Date startDate = new Date();
 	private Date currentDate = new Date();
 	private TechnicalAnalysisView taview;
 	private TechnicalAnalysis tamodel;
-	
+	private ActionListener updateBtnListener;
+	private ActionListener checkBoxesListener;
+	private ActionListener menuExitListner;
+
 	public TechnicalAnalysisController(){
 		this.tamodel = new TechnicalAnalysis();
 		defaultTimeperiod();
 		this.taview = new TechnicalAnalysisView(tamodel.getStockList());
 		setChart();
 		this.taview.setVisible(true);
-		this.taview.AddBtnActionPerformed(new MAListener());
+//		this.taview.AddBtnActionPerformed(new MAListener());
+
+		//Listeners
+		updateBtnListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Date[] timePeriod = taview.getDates();
+				taview.setLabelText(taview.getStockSelected() + " from: " + timePeriod[0] + " to: " + timePeriod[1]);
+			}
+		};
+		taview.addUdateBtnListner(updateBtnListener);
+
+		checkBoxesListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String el = ((AbstractButton) e.getSource()).getName().toString();
+				taview.setLabelText(el);
+			}
+		};
+		taview.addCheckboxesListner(checkBoxesListener);
+
+		menuExitListner = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		};
+		taview.addMenuExitListner(menuExitListner);
 	}
-	
+
+	public static void main(String[] arg) {
+		//String[] time = {"01,28,2016", "02,28,2017"};
+		//TechnicalAnalysisController ta = new TechnicalAnalysisController();
+	}
+
 	private void setChart(){
-		String stock = this.taview.getStock();
+		String stock = this.taview.getStockSelected();
 		System.out.print("Chechpoint stock" + stock);
 		JFreeChart jfree = this.tamodel.performAnalysis(stock, toString(startDate), toString(currentDate));
 		this.taview.setChart(jfree);
 	}
-	
+
 	private void defaultTimeperiod(){
 		Calendar cal = Calendar.getInstance();
 		currentDate = cal.getTime();
 		cal.add(Calendar.YEAR, -1); // http://stackoverflow.com/questions/14946886/store-current-date-and-date-1-year-from-current-in-java
 		startDate = cal.getTime();
 	}
-	
-	private class MAListener implements ActionListener{
-		
-		public void actionPerformed(ActionEvent e) {
-			//int maValue = taview.getSpinnerValue();
-			tamodel.addMA(21);
-		}
-		
-	}
-	
+
+
 	private String toString(Date date){
 		DateFormat df = new SimpleDateFormat("MM,dd,yyyy");
 		String strDate  = df.format(date);		
 		return strDate;
 	}
-	
-	public static void main(String[] arg){
-		//String[] time = {"01,28,2016", "02,28,2017"};
-		//TechnicalAnalysisController ta = new TechnicalAnalysisController();
+
+	private class MAListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			//int maValue = taview.getSpinnerValue();
+			tamodel.addMA(21);
+		}
 	}
 }
